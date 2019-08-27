fe6b5a3c71f8b1b47e70eab63b89cbedbfd08a7a
MuthomiGuantai <muthomiguantai@gmail.com>
2019-08-27 11:12:20 +0300

    adding news and news-single pages

---
+++ src/app/news.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+const API_KEY = environment.apiKey;
+const API_URL = environment.apiUrl;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+  currentArticle: any;
+
+  constructor(private http: HttpClient) { }
+
+getData(url){
+  return this.http.get(`${API_URL}/${url}&apikey=${API_KEY}`);
+}
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://newsapi.org/v2',
+  apiKey: 'f9a55958d1714abc988947a71b4993dd'
 };
 
 /*
